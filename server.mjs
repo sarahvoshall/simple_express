2df48114b14013ea5c7d535a688df6e80108c00b
Sarah Voshall <sarahvoshall@gmail.com>
2024-01-20 22:19:39 -0800

    add emojis

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
+  res.send("🌺💐🌹🌷🌻 Welcome to Sarah's site! 🌺💐🌹🌷🌻");
 });
 
 app.listen(port, () => {
